77a5761f31f8dabbfcad782e411cf61db70510a3
AdamArthurFaizal <adam.faizal.af6@gmail.com>
2020-03-02 19:21:02 +0700

    Mereview kodingan ku sendiri wkwk

--- ProceduralProgramming/52SistemAuth/functions/user.php
+++ ProceduralProgramming/52SistemAuth/functions/user.php
@@ -21,22 +21,25 @@ function cek_data($user, $pass){
     global $link;
 //    $user = escape($user);
     $query = "select password from users where username = '$user'";
-    $hasil = mysqli_query($link, $query);
-    $hash = mysqli_fetch_assoc($hasil)['password'];
 //    die('username = '.$user.', password ='.$pass.', query = '.$query. ', hash = '.$hash);
-
-    if (password_verify($pass, $hash)) return true;
-    else return false;
+    if ($hasil = mysqli_query($link, $query)) {
+        if ($hash = mysqli_fetch_assoc($hasil)['password']) {
+            if (password_verify($pass, $hash)) return true;
+            else return false;
+        }
+    }
 }
 
 function cek_status($user){
     global $link;
     $user = escape($user);
     $query = "select role from users where username = '$user'";
-    $hasil = mysqli_query($link, $query);
-    $status = mysqli_fetch_assoc($hasil)['role'];
-    if ($status == 1) return true;
-    else return false;
+    if ($hasil = mysqli_query($link, $query)) {
+        if ($status = mysqli_fetch_assoc($hasil)['role']) {
+            if ($status == 1) return true;
+            else return false;
+        }
+    }
 }
 
 function escape($data){
