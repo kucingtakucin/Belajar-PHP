74a7d8534fd6e98140205f8836faabce7b30a556
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-06-10 18:32:24 +0700

    Sekarang sampai ke pertemuan 3

---
+++ ProceduralProgramming/phpdasar/htdocs/pertemuan2/index.php
@@ -0,0 +1,7 @@
+<?php
+$nama = 'Adam';
+$panggil = 'Arthur';
+$nama_belakang = 'Faizal';
+$umur = 17;
+echo "Halo, nama saya $nama, biasa dipanggil $panggil, umur saya $umur tahun";
+echo "Jadi nama lengkap saya adalah $nama $panggil $nama_belakang";
---
+++ ProceduralProgramming/phpdasar/htdocs/pertemuan3/latihan1.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Latihan 1</title>
+</head>
+<body>
+<h1>Hello, World!</h1>
+<?php
+$nama_depan = 'Adam';
+$nama_tengah = 'Arthur';
+$nama_belakang = 'Faizal';
+echo "Halo, perkenalkan nama saya $nama_depan $nama_tengah $nama_belakang";
+?>
+<table border="1" cellpadding="10" cellspacing="0">
+    <?php for ($i = 1; $i <= 3; $i++) :?>
+        <tr>
+            <?php for ($j = 1; $j <= 5; $j++) :?>
+                <td><?php echo "$i, $j"?></td>
+            <?php endfor ?>
+        </tr>
+    <?php endfor ?>
+</table>
+</body>
+</html>
