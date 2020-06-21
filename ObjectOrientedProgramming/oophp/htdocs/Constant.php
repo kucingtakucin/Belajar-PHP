f6ffb087f69f48e5b83ff0b00a84dd2e5716e5c8
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-06-21 21:27:40 +0700

    Menyelesaikan constant

---
+++ ObjectOrientedProgramming/oophp/htdocs/Constant.php
@@ -0,0 +1,15 @@
+<?php
+define('NAMA', 'Adam Arthur Faizal');
+class Constant {
+    const HALO = 'Halooo gaess';
+}
+
+echo NAMA;
+echo '<br>';
+echo Constant::HALO;
+echo '<br>';
+echo __DIR__;
+echo '<br>';
+echo __FILE__;
+echo '<br>';
+echo __TRAIT__;
